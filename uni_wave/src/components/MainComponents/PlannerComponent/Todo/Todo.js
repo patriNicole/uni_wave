05a41cbf0116fe5f94cdfa458d55d98194183dfd
Patricia-Nicole Gal <patricia-nicole@Patricia-Nicoles-MacBook-Pro.local>
2022-11-24 00:15:53 +0000

    ToDo Categories Added

--- uni_wave/src/components/MainComponents/PlannerComponent/Todo/Todo.js
+++ uni_wave/src/components/MainComponents/PlannerComponent/Todo/Todo.js
@@ -8,23 +8,27 @@ const Todo = (props) => {
   const [edit, setEdit] = useState({
     id: null,
     value: "",
+    category: ""
   });
 
+  //console.log(edit)
+
   const submitUpdate = (value) => {
-    props.updateTodo(edit.id, value);
+    props.updateTodo(edit.id, value, props.showCategory);
     //prepare value for next editing
     setEdit({
       id: null,
       value: "",
+      category: ""
     });
   };
 
   //if we want to edit an item, then add a new form for editing
   if (edit.id) {
-    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+    return <TodoForm edit={edit} onSubmit={submitUpdate} showCategory={props.showCategory} setCategory={props.setCategory}/>;
   }
 
-  return props.todos.map((todo, index) => (
+  return props.todoFiltered.map((todo, index) => (
     <div
       className={todo.isComplete ? "todo-row complete" : "todo-row"}
       key={index}
@@ -38,7 +42,7 @@ const Todo = (props) => {
           className="delete-iconTodo"
         />
         <TiEdit
-          onClick={() => setEdit({ id: todo.id, value: todo.text })}
+          onClick={() => setEdit({ id: todo.id, value: todo.text, category: todo.category })}
           className="edit-iconTodo"
         />
       </div>
