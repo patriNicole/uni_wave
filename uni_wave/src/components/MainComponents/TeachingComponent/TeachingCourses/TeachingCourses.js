f10be6284b7b17e547bac1085027a75d51d90e05
Patricia-Nicole Gal <patricia-nicole@Patricia-Nicoles-MacBook-Pro.local>
2023-03-02 00:31:09 +0000

    Teaching Style Courses Component

--- uni_wave/src/components/MainComponents/TeachingComponent/TeachingCourses/TeachingCourses.js
+++ uni_wave/src/components/MainComponents/TeachingComponent/TeachingCourses/TeachingCourses.js
@@ -2,9 +2,72 @@ import React from "react";
 import "./TeachingCourses.css";
 
 export default function TeachingCourses() {
-    return (
-        <div className="teachingCourse">
-            Teaching Courses
+  return (
+    <div className="teachingCourse">
+      <div class="containerTeachingCourses">
+        
+        <div class="itemCourse">
+          <h1> Course </h1>
+          <div class="detailsCourse">
+            <h4> Object 1</h4>
+            <h4> Object 2</h4>
+          </div>
         </div>
-    );
+
+        <div class="itemCourse">
+          <h1> Course </h1>
+          <div class="detailsCourse">
+            <h4> Object 1</h4>
+            <h4> Object 2</h4>
+            <h4> Object 3</h4>
+            <h4> Object 4</h4>
+            <h4> Object 5</h4>
+          </div>
+        </div>
+        <div class="itemCourse">
+          <h1> Course </h1>
+          <div class="detailsCourse">
+            <h4> Object 1</h4>
+          </div>
+        </div>
+        <div class="itemCourse">
+          <h1> Course </h1>
+          <div class="detailsCourse">
+            <h4> Object 1</h4>
+            <h4> Object 2</h4>
+          </div>
+        </div>
+        <div class="itemCourse">
+          <h1> Course </h1>
+          <div class="detailsCourse">
+            <h4> Object 1</h4>
+            <h4> Object 2</h4>
+            <h4> Object 3</h4>
+            <h4> Object 4</h4>
+          </div>
+        </div>
+
+      <div class="itemCourse">
+          <h1> Course </h1>
+          <div class="detailsCourse">
+            <h4> Object 1</h4>
+            <h4> Object 2</h4>
+            <h4> Object 3</h4>
+            <h4> Object 4</h4>
+          </div>
+        </div>
+        <div class="itemCourse">
+          <h1> Course </h1>
+          <div class="detailsCourse">
+            <h4> Object 1</h4>
+            <h4> Object 2</h4>
+            <h4> Object 3</h4>
+            <h4> Object 4</h4>
+          </div>
+        </div>
+      
+      
+        </div>
+    </div>
+  );
 }
