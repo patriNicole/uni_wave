58acd2e2a0e340fbd608f743cf04861f34d864ee
Patricia-Nicole Gal <patricia-nicole@Patricia-Nicoles-MacBook-Pro.local>
2023-03-21 19:31:12 +0000

    Info about Main Building

--- uni_wave/src/components/MainComponents/MapComponent/MapInfoComponents/MapInfoUniversityPlace.js
+++ uni_wave/src/components/MainComponents/MapComponent/MapInfoComponents/MapInfoUniversityPlace.js
@@ -1,11 +1,29 @@
 import React from "react";
 import "../MapComponent.css";
 
+import { FiMapPin } from "react-icons/fi";
+import UniversityPlace from "../MapInfoPDF/UniversityPlace.pdf";
+
 export default function MapInfoUniversityPlace(props) {
+  const style = { color: "white", fontSize: "1.5em" };
 
   return (
     <>
-      University Place
+      <p className="infoMapTitle">University Place </p>
+
+      <a
+        href="https://www.google.com/maps/place/The+University+of+Manchester/@53.4656642,-2.2363802,15z/data=!4m5!3m4!1s0x0:0x7cb93350c67efb3c!8m2!3d53.4668498!4d-2.2338837?hl=en-GB"
+        target="_blank"
+      >
+        <FiMapPin size={20} style={style} />
+      </a>
+
+      <iframe
+        src={UniversityPlace}
+        height="100%"
+        width="100%"
+        className="iframeBuildings"
+      ></iframe>
     </>
   );
 }
